6689bc167d44de65402eeca70f857b6c890be70b
starwatcher <redmoonloon@outlook.com>
2022-12-13 23:06:03 +1000

    add generate-react-cli and create checbox (empty)

---
+++ src/front/src/components/checkbox/checkbox.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./style.module.css";
+
+export type CheckboxProps = {};
+
+export const Checkbox: React.FC<CheckboxProps> = (props) => {
+  return <div></div>;
+};

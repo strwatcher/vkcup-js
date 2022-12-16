fd08b15c3dc47d60cda299d51ff9e6d64f7fa300
starwatcher <redmoonloon@outlook.com>
2022-12-16 22:00:21 +1000

    open image in new window from big preview fix styles of attachments popup

--- src/front/src/components/elements/attachment-item/attachment-item.tsx
+++ src/front/src/components/elements/attachment-item/attachment-item.tsx
@@ -10,12 +10,22 @@ export type IAttachmentItem = {
 export type AttachmentItemProps = IAttachmentItem;
 
 export const AttachmentItem: React.FC<AttachmentItemProps> = (props) => {
+  const openImageInNewWindow = React.useCallback(() => {
+    const newWindow = window.open("about:blank");
+    const image = newWindow!.document.createElement("img");
+    image.src = props.bytes;
+    newWindow?.document.body.appendChild(image);
+  }, [props.name, props.bytes]);
+
   return (
     <div className={s.wrapper}>
       <img src={props.bytes} className={s.preview} />
       <span className={s.info}>
         {`${props.name}.jpg ${bToMb(base64Size(props.bytes)).toFixed(2)} MB`}
       </span>
+      <div className={s.bigPreview}>
+        <img src={props.bytes} onClick={openImageInNewWindow} />
+      </div>
     </div>
   );
 };
